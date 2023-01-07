dc350413875bca76d252453f6e1b94cba0bf3904
ubuntu <enricoruggiano1@gmail.com>
2023-01-07 12:10:46 +0100

    [add] https://tenor.com/view/norman-osborn-spider-man-smile-gif-24532865

---
+++ commands/saluta.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('saluta')
+		.setDescription('Saluta la gente'),
+	async execute(interaction) {
+        console.log(interaction.user);
+		await interaction.reply(`Bella ${interaction.user.username}!`);
+        await wait(2000);
+        await interaction.followUp("https://tenor.com/view/norman-osborn-spider-man-smile-gif-24532865");
+    },
+};
